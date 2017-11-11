26abf177473be29c15077d8cd8b011aaa48378b5
Karyn Vo <vkaryn@gmail.com>
2017-11-10 22:59:52 -0700

    Basic tooltip

--- index.js
+++ index.js
@@ -5,6 +5,12 @@ var idCountryMapping = d3.map();
 var selectCountry;
 var nestByCountryAndYear;
 
+lineTypeEnum = {
+    MIN : 0,
+    MEAN : 1,
+    MAX : 2
+}
+
 /* create svg */
 var margin = { top: 50, right: 50, bottom: 100, left: 100 },
 	width = 960 - margin.left - margin.right,
@@ -30,6 +36,8 @@ var xAxis = d3.axisBottom(x)
 var yAxis = d3.axisLeft(y)
 			.tickFormat(d3.format(".3p"));
 
+var textBox = d3.select("#tooltip");
+
 /* parse file */
 d3.text("IHME_GBD_2013_OBESITY_PREVALENCE_1990_2013_Y2014M10D08.csv", function (error, data) {
 	if(error){
@@ -48,6 +56,8 @@ d3.text("IHME_GBD_2013_OBESITY_PREVALENCE_1990_2013_Y2014M10D08.csv", function (
 		})
 		.key(function(d) { return d.year; })
 		.entries(csvData);
+
+	d3.select(".loaderPosition").remove();
 
 	if(debug){
 		console.log(nestByCountryAndYear);
@@ -139,19 +149,19 @@ var selectCountry = function(countryCode){
 var line = d3.line()
 		.x(function(d){ return x(new Date(d.key, 0, 1, 0)); })
 		.y(function(d){
-			return getYValue(d.values, "mean");
+			return y(getMean(d.values, lineTypeEnum.MEAN));
 		});
 
 var lineMin = d3.line()
 		.x(function(d){ return x(new Date(d.key, 0, 1, 0)); })
 		.y(function(d){ 
-			return getYValue(d.values, "min");
+			return y(getMean(d.values, lineTypeEnum.MIN));
 		});
 
 var lineMax = d3.line()
 		.x(function(d){ return x(new Date(d.key, 0, 1, 0)); })
 		.y(function(d){ 
-			return getYValue(d.values, "max");
+			return y(getMean(d.values, lineTypeEnum.MAX));
 		});
 
 var findCountry = function(){
@@ -179,15 +189,15 @@ var findMinOrMaxMean = function(getMin){
 	return getMin ? d3.min(a) : d3.max(a);
 }
 
-var getYValue = function(dataForAYear, type){
-	return y(d3.mean(dataForAYear, function(element){
-		if(type == "mean")
+var getMean = function(dataForAYear, type){
+	return d3.mean(dataForAYear, function(element){
+		if(type == lineTypeEnum.MEAN)
 			return element["mean"];
-		if(type == "min")
+		if(type == lineTypeEnum.MIN)
 			return element["lower"];
-		if(type == "max")
+		if(type == lineTypeEnum.MAX)
 			return element["upper"];
-	}));
+	});
 }
 
 var createNewPath = function(){
@@ -199,15 +209,15 @@ var createNewPath = function(){
 var drawLines = function(){
 	createNewPath()
 		.attr("d", line(selectedCountry.values))
-		.attr("class", "meanColor");
+		.attr("class", "line meanColor");
 
 	createNewPath()
 		.attr("d", lineMin(selectedCountry.values))
-		.attr("class", "minColor");
+		.attr("class", "line minColor");
 
 	createNewPath()
 		.attr("d", lineMax(selectedCountry.values))
-		.attr("class", "maxColor");
+		.attr("class", "line maxColor");
 }
 
 var createNewPoints = function(){
@@ -216,19 +226,58 @@ var createNewPoints = function(){
 		.selectAll("circle")
 		.data(selectedCountry.values)
 		.enter().append("circle")
-		.attr("class", "point")
 		.attr("cx", function(d){ return x(new Date(d.key, 0, 1, 0)); })
-		.attr("r", 7);
+		.on("mouseover", function(d){
+			d3.select(this)
+				.classed("nonHoverPoint", false)
+				.classed("hoverPoint", true);
+
+			textBox
+				.classed("invisibleTooltip", false)
+				.classed("visibleTooltip", true);
+
+			textBox
+				.style("left", (d3.event.pageX) - 20 + "px")		
+                .style("top", (d3.event.pageY) - 60 + "px");
+
+            textBox
+				.select("#percentage")
+				.text(d3.format(".3p")(getText(this.className["baseVal"], d.values)));
+
+            textBox
+				.select("#header")
+				.text(selectedCountry.key);
+		})
+		.on("mouseout", function(d){
+			d3.select(this)
+				.classed("hoverPoint", false)
+				.classed("nonHoverPoint", true);
+
+			textBox
+				.classed("visibleTooltip", false)
+				.classed("invisibleTooltip", true);
+		});
 }
 
 var drawPoints = function(){
 	createNewPoints()
-		.attr("cy", function(d){ return getYValue(d.values, "mean"); });
+		.attr("cy", function(d){ return y(getMean(d.values, lineTypeEnum.MEAN)); })
+		.attr("class", "point nonHoverPoint meanColor");
 
 	createNewPoints()
-		.attr("cy", function(d){ return getYValue(d.values, "min"); });
+		.attr("cy", function(d){ return y(getMean(d.values, lineTypeEnum.MIN)); })
+		.attr("class", "point nonHoverPoint minColor");
 
 	createNewPoints()
-		.attr("cy", function(d){ return getYValue(d.values, "max"); });
+		.attr("cy", function(d){ return y(getMean(d.values, lineTypeEnum.MAX)); })
+		.attr("class", "point nonHoverPoint maxColor");
 }
 
+var getText = function(classes, dataForAYear){
+	if(classes.includes("mean"))
+		return getMean(dataForAYear, lineTypeEnum.MEAN);
+	if(classes.includes("min"))
+		return getMean(dataForAYear, lineTypeEnum.MIN);
+	if(classes.includes("max"))
+		return getMean(dataForAYear, lineTypeEnum.MAX);
+}
